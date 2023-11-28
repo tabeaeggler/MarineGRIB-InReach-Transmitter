e0a6a29ce64c5812d63318f3e36ed2dfd97daefd
Tabea <35971471+tabeaeggler@users.noreply.github.com>
2023-11-28 19:20:56 +0000

    Refactoring  main.py: improved None check

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ if __name__ == "__main__":
         print('Checking...', flush=True)
 
         # check for new messages and retrieve GRIB path and Garmin reply URL
-        grib_path, garmin_reply_url = email_func.process_new_inreach_message(auth_service)
+        result = email_func.process_new_inreach_message(auth_service)
 
         # if a new message is received
-        if grib_path is not None:
+        if result is not None:
+            grib_path, garmin_reply_url = result
+            print("x", grib_path)
+
             # encode GRIB to binary
             encoded_grib = saildoc_func.encode_saildocs_grib_file(grib_path)
+            print("xx", encoded_grib)
 
             # send the encoded GRIB to InReach
             inreach_func.send_messages_to_inreach(garmin_reply_url, encoded_grib)
+            print("xxx", encoded_grib)
 
         # wait for the next check in 60 seconds
         time.sleep(60)
